95465f73a81af04495a1fd63e799bbd7735a117a
Josh Bivens <hotbioform@gmail.com>
2016-03-03 22:59:08 -0700

    Create CreatorComponent

--- src/creator-component/creator-component.js
+++ src/creator-component/creator-component.js
@@ -1,18 +1,53 @@
 import Vue from 'vue';
 import template from './creator-component-template.html';
+import StatsComponent from '../stats-component/stats-component';
 
 const CreatorComponent = Vue.extend({
   template,
+  components: {
+    'stats-component': StatsComponent
+  },
   data() {
     return {
-      characterName: '',
-      caste: '',
-      created: false
+      character: {
+        characterName: '',
+        caste: '',
+        hp: 100,
+        strength: 0,
+        agility: 0,
+        luck: 0,
+        mana: 0,
+        created: false
+      }
     }
   },
   methods: {
     create() {
-      this.created = true;
+      this.character.created = true;
+      if (this.character.caste == "Warrior") {
+        this.character.strength = 80;
+        this.character.agility = 30;
+        this.character.luck = 30;
+        this.character.mana = 10;
+      }
+      if (this.character.caste == "Ranger") {
+        this.character.strength = 40;
+        this.character.agility = 70;
+        this.character.luck = 50;
+        this.character.mana = 30;
+      }
+      if (this.character.caste == "Rogue") {
+        this.character.strength = 50;
+        this.character.agility = 60;
+        this.character.luck = 40;
+        this.character.mana = 30;
+      }
+      if (this.character.caste == "Mage") {
+        this.character.strength = 30;
+        this.character.agility = 30;
+        this.character.luck = 30;
+        this.character.mana = 70;
+      }
     }
   }
 });
