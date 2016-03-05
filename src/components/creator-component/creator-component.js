fddb621fe8c513aaea996bb343db0031ed8a1529
Josh Bivens <hotbioform@gmail.com>
2016-03-05 13:28:19 -0700

    Require character name

--- src/components/creator-component/creator-component.js
+++ src/components/creator-component/creator-component.js
@@ -6,30 +6,34 @@ const CreatorComponent = Vue.extend({
   props: ['character'],
   methods: {
     create() {
-      this.character.created = true;
-      if (this.character.caste == "Cavalier") {
-        this.character.strength = 80;
-        this.character.agility = 30;
-        this.character.luck = 30;
-        this.character.mana = 10;
-      }
-      if (this.character.caste == "Hunter") {
-        this.character.strength = 40;
-        this.character.agility = 70;
-        this.character.luck = 40;
-        this.character.mana = 20;
-      }
-      if (this.character.caste == "Assassin") {
-        this.character.strength = 50;
-        this.character.agility = 60;
-        this.character.luck = 40;
-        this.character.mana = 30;
-      }
-      if (this.character.caste == "Shaman") {
-        this.character.strength = 30;
-        this.character.agility = 30;
-        this.character.luck = 30;
-        this.character.mana = 70;
+      if (this.character.name) {
+        this.character.created = true;
+        if (this.character.caste == "Cavalier") {
+          this.character.strength = 80;
+          this.character.agility = 30;
+          this.character.luck = 30;
+          this.character.mana = 10;
+        }
+        if (this.character.caste == "Hunter") {
+          this.character.strength = 40;
+          this.character.agility = 70;
+          this.character.luck = 40;
+          this.character.mana = 20;
+        }
+        if (this.character.caste == "Assassin") {
+          this.character.strength = 50;
+          this.character.agility = 60;
+          this.character.luck = 40;
+          this.character.mana = 30;
+        }
+        if (this.character.caste == "Shaman") {
+          this.character.strength = 30;
+          this.character.agility = 30;
+          this.character.luck = 30;
+          this.character.mana = 70;
+        }
+      } else {
+        alert("Please enter a name for your character")
       }
     }
   }
